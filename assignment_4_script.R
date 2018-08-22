7c61a6e4eb4602f427fceae94f2ac8c3189d62e8
ascheunemann <40202401+ascheunemann@users.noreply.github.com>
2018-08-22 12:31:24 +0200

    Add files via upload
    The latest version of Assignment 4

--- assignment_4_script.R
+++ assignment_4_script.R
@@ -3,6 +3,9 @@ library(readxl)
 library(psych)
 library(lattice)
 library(car)
+library(ppcor)
+library(vcd)
+library(polycor)
 
 load("data/eye_FR_testdata-1")
 load("data/eye_FR_traindata-1")
@@ -45,22 +48,28 @@ ggplot(traindata, aes(lineupacc, confidence)) +
   geom_jitter(alpha = .4) +
   facet_grid(~lineupacc)
 
+ggplot(traindata, aes(lineupacc, lineuprt)) +
+  geom_boxplot() +
+  geom_jitter(alpha = .4) +
+  facet_grid(~lineupacc)
+
+ggplot(traindata, aes(lineupacc, facecomparison)) +
+  geom_boxplot() +
+  geom_jitter(alpha = .4) +
+  facet_grid(~lineupacc)
+
+ggplot(traindata, aes(lineupacc, automatic)) +
+  geom_boxplot() +
+  geom_jitter(alpha = .4) +
+  facet_grid(~lineupacc)
 
 traindata %>% 
-  select(lineupacc, confidence, lineuprt, automatic, facecomparison) %>% 
+  dplyr::select(lineupacc, confidence, lineuprt, automatic, facecomparison) %>% 
   describeBy("lineupacc")
 
 biserial(traindata[,5:8], traindata[2:4])
 
-install.packages("polycor")
-library(polycor)
-install.packages("ppcor")
-library(ppcor)
-library(vcd)
 hetcor(traindata[,2:8])
-
-
-## Question 4
 
 mosaic(~ lineupacc + lineuptpta + exposure, data = traindata, main = "Stats", shade = T, legend = T)
 
@@ -70,15 +79,65 @@ null <- glm(lineupacc ~ lineuptpta + exposure, family = "binomial", data = train
 summary(null)
 
 
-model1.train <- glm(lineupacc ~ lineuptpta*facecomparison + lineuptpta* automatic + exposure + confidence, family = "binomial", data = traindata)
+model1.train <- glm(lineupacc ~ lineuptpta*facecomparison + lineuptpta*automatic + exposure + confidence, family = "binomial", data = traindata)
 summary(model1.train)
 
-model1.test <- glm(lineupacc ~ lineuptpta*facecomparison + lineuptpta* automatic + exposure + confidence, family = "binomial", data = testdata)
+model1.test <- glm(lineupacc ~ lineuptpta*facecomparison + lineuptpta* automatic + exposure + as.numeric(confidence), family = "binomial", data = testdata)
 summary(model1.test)
+
+testdata$lineuptpta <- tolower(testdata$lineuptpta)
+
+model2.test <- predict(model1.train, newdata = testdata)
+summary(model2.test)
+
+anova(model1.train, null)
 
 1-pchisq(null$deviance,model1.train$df.residual)
 
-anova(null, model1.train)
+str(testdata)
+
+## Question 5
+
+model2 <- glm(lineupacc ~ confidence + lineuptpta, family = "binomial", data = traindata)
+summary(model2)
+
+predict.tp <- data.frame(confidence = 80, lineuptpta = "tp")
+
+predict(model2, newdata = predict.tp)
+
+predict(model2, 
+        newdata = predict.tp) # logit
+predict(model2, 
+        newdata = predict.tp, type = "response") # probability
+x <- predict(model2, 
+             newdata = predict.tp, type="response")
+odds.tp <- x/(1-x)  # to get the answer in odds
+odds.tp
+
+predict.ta <- data.frame(confidence = 80, lineuptpta = "ta")
+
+predict(model2, newdata = predict.ta)
+
+predict(model2, 
+        newdata = predict.ta) # logit
+predict(model2, 
+        newdata = predict.ta, type = "response") # probability
+x <- predict(model2, 
+             newdata = predict.ta, type="response")
+odds.ta <- x/(1-x)  # to get the answer in odds
+odds.ta
+
+
+exp(coefficients(model2))
+
+install.packages("caret")
+library(caret)
+
+confusionMatrix(as.factor(as.numeric(pred.prob >= 0.5)), as.factor(traindata$lineupacc))
+
+confusionMatrix(as.factor(traindata$lineupacc), as.factor(as.numeric(pred.prob >= 0.5)))
+
+
 
 
 ## Question 5
